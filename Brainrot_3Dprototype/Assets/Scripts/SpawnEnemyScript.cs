53c4e0df589f67bbccf6274ee8c9af8b50ac647b
Ava-TU <C24384981@myTUDublin.ie>
2025-12-11 15:41:40 +0000

    now has a counter for the spawner
    the spawner now counts how many enemies have been spawned into the scene

--- Brainrot_3Dprototype/Assets/Scripts/SpawnEnemyScript.cs
+++ Brainrot_3Dprototype/Assets/Scripts/SpawnEnemyScript.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 
 public class SpawnEnemyScript : MonoBehaviour
 {
-    public GameObject followEnemyPrefab; //This is the object that I want to spawn in
-    public Vector3 spawnPosition; //This will select where I want to spawn the prefab
+    public GameObject prefab; //This is the object that I want to spawn in
+    public GameObject spawnPosition; //This will select where I want to spawn the prefab
+    //If i wanted to chose a specific position, making a Vector3 variable would allow me to change the transforms X,Y and Z position in the inspector
+
+    public int prefabCount; //This will be used to store the amount of prefabs that have been spawned into the scene
 
 
     public void SpawnEnemy()
     {
-        Instantiate(followEnemyPrefab, spawnPosition, Quaternion.identity); //Instantiates the prefab by getting the GameObject, it's position and the Quaternion.identity gives it no rotation
+        Instantiate(prefab, spawnPosition.transform.position, Quaternion.identity); //Instantiates the prefab by getting the GameObject, it's position and the Quaternion.identity gives it no rotation
+        prefabCount += 1; //Adds 1 to the count variable
+
+        if (prefabCount > 1)
+        {
+
+        }
     }
 }
