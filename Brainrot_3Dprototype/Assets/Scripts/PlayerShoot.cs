a58487d6cfed54e3db262bf7809d914b3a20a646
Ava-TU <C24384981@myTUDublin.ie>
2025-12-11 17:42:28 +0000

    bullet firerate working
    bullet is now limited to its fire rate, e.g. if the fire rate is 5 then the player will shoot 5 bullets per second

--- Brainrot_3Dprototype/Assets/Scripts/PlayerShoot.cs
+++ Brainrot_3Dprototype/Assets/Scripts/PlayerShoot.cs
@@ -13,18 +13,25 @@ public class PlayerShoot : MonoBehaviour
     public GameObject bulletPrefab; //This calls the bullets prefab
     public KeyCode shootKey;
 
+    private float timer;
+
     private void Update()
     {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime / fireRate; //This makes it so that the player will shoot at whatever the fire rate is per second, e.g. if the fire rate is 2, then it will shoot 2 bullets per second
+        }
+
         if (isAuto)
         {
-            if (Input.GetKey(shootKey)) //Happens even when the key is held down
+            if (Input.GetKey(shootKey) && timer <= 0) //Happens even when the key is held down
             {
-
+                Shoot();
             }
         }
         else
         {
-            if (Input.GetKeyDown(shootKey)) //Only happens when the key is clicked
+            if (Input.GetKeyDown(shootKey) && timer <= 0) //Only happens when the key is clicked
             {
                 Shoot();
             }
@@ -35,5 +42,7 @@ public class PlayerShoot : MonoBehaviour
     {
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawnTransform.position, Quaternion.identity);
         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawnTransform.forward * bulletSpeed, ForceMode.Impulse); //The impulse force mode makes sure that the force added to the bullet is instant
+
+        timer = 1; //This resets the timer back to one
     }
 }
