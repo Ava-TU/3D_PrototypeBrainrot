d0fff8d9bad1ca0fd865c4053a9bf360e0dc4ad0
Ava-TU <C24384981@myTUDublin.ie>
2025-12-11 17:04:54 +0000

    creating bullet

---
+++ Brainrot_3Dprototype/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    [Header("Bullet Variables")]
+    public float bulletSpeed; //How fast the bullets move
+    public float fireRate, bulletDamage;
+    public bool isAuto; //This will toggle if the gun is automatic or not
+
+    [Header("Initial Setup")]
+    public Transform bulletSpawnTransform; //This is where the bullets will spawn from
+    public GameObject bulletPrefab; //This calls the bullets prefab
+    public KeyCode shootKey;
+
+    private void Update()
+    {
+        if (isAuto)
+        {
+            if (Input.GetKey(shootKey)) //Happens even when the key is held down
+            {
+
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(shootKey)) //Only happens when the key is clicked
+            {
+
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnTransform.position, Quaternion.identity);
+    }
+}
--- Brainrot_3Dprototype/Assets/Scripts/SpawnEnemyScript.cs
+++ Brainrot_3Dprototype/Assets/Scripts/SpawnEnemyScript.cs
@@ -1,9 +1,10 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnEnemyScript : MonoBehaviour
 {
     public GameObject prefab; //This is the object that I want to spawn in
-    public GameObject spawnPosition; //This will select where I want to spawn the prefab
+    public Transform spawnPosition; //This will select where I want to spawn the prefab
     //If i wanted to chose a specific position, making a Vector3 variable would allow me to change the transforms X,Y and Z position in the inspector
 
     public int prefabCount; //This will be used to store the amount of prefabs that have been spawned into the scene
@@ -11,12 +12,7 @@ public class SpawnEnemyScript : MonoBehaviour
 
     public void SpawnEnemy()
     {
-        Instantiate(prefab, spawnPosition.transform.position, Quaternion.identity); //Instantiates the prefab by getting the GameObject, it's position and the Quaternion.identity gives it no rotation
+        Instantiate(prefab, spawnPosition.position, Quaternion.identity); //Instantiates the prefab by getting the GameObject, it's position and the Quaternion.identity uses the rotation thats used in the prefab
         prefabCount += 1; //Adds 1 to the count variable
-
-        if (prefabCount > 1)
-        {
-
-        }
     }
 }
